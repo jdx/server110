188c2e66d2c1d04258270531a1beacfeda360c89
dickeyxxx <jeff@dickey.xxx>
2012-12-04 23:22:38 -0800

    fix migration

--- db/migrate/20121205070340_add_date.rb
+++ db/migrate/20121205070340_add_date.rb
@@ -3,6 +3,7 @@ class AddDate < ActiveRecord::Migration
     add_column :tasks, :date, :date
     Task.find_each do |t|
       t.date = t.created_at
+      t.save!
     end
     change_column :tasks, :date, :date, null: false
   end
